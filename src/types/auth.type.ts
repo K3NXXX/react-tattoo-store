2a8ac4d4332245417935855ef3459632255eb4ad
K3NXXX <xxvolodyax7@gmail.com>
2024-11-05 13:34:59 +0200

    remove error in type

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -5,10 +5,7 @@ export interface IUser {
   surname: string;
   email: string;
   phone_number: string;
-<<<<<<< HEAD
   password: string;
-=======
->>>>>>> 406fe2bc5b76af57ea88ed5711a33f5ac4650976
   role: string;
   cart: [];
   favorites: string[];
