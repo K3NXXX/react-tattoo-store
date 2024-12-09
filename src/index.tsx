315420483e9e0f44cb3be043b718f583a2268ad2
K3NXXX <xxvolodyax7@gmail.com>
2024-12-09 23:30:19 +0200

    changed vercel json file

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,13 @@ import { store } from './redux/store'
 const rootElem = document.getElementById('root')
 const queryClient = new QueryClient()
 
-const baseName =
-process.env.NODE_ENV === "production" ? "/react-tattoo-store" : "";
 
 if (rootElem) {
 	const root = ReactDOM.createRoot(rootElem)
 	root.render(
 		<QueryClientProvider client={queryClient}>
 			<Provider store={store}>
-			<BrowserRouter basename={baseName}>
+			<BrowserRouter >
 					<App />
 				</BrowserRouter>
 			</Provider>
